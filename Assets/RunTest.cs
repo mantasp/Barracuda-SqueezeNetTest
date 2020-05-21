efbe714d3a1b135e0c2e494b02ef17d221a41f00
Mantas Puida <mantas@unity3d.com>
2020-05-21 19:13:52 +0300

    Initial commit

---
+++ Assets/RunTest.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Barracuda;
+//using Barracuda;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.UI;
+
+public class RunTest : MonoBehaviour
+{
+	public float epsilon = 1e-3f;
+	public NNModel srcModel;
+	public Texture2D inputImage;
+	public TextAsset labelsAsset;
+	public int inputResolutionY = 224;
+	public int inputResolutionX = 224;
+
+	public bool useGPU = true;
+	
+	public Text text;
+	public RawImage displayImage;
+
+	public int repeatExecution = 1;
+
+	private Model model;
+	private IWorker engine;
+	private Dictionary<string, Tensor> inputs = new Dictionary<string, Tensor>();
+	private string[] labels;
+	
+	private float averageDt;
+	private float rawAverageDt;
+	private string output_name = "output_softmax";
+
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		Application.targetFrameRate = 60;
+		
+		labels = labelsAsset.text.Split('\n');
+		model = ModelLoader.Load(srcModel, false);
+		
+		// Append Softmax() layer to the end of the network
+		var modelBuilder = new ModelBuilder(model);
+		modelBuilder.Softmax(output_name, model.outputs[0]);
+		modelBuilder.Output(output_name);
+		
+		engine = WorkerFactory.CreateWorker(model, useGPU ? WorkerFactory.Device.GPU : WorkerFactory.Device.CPU);
+
+		displayImage.texture = PrepareTextureForInput(inputImage, !useGPU);
+		var input = new Tensor(PrepareTextureForInput(inputImage, !useGPU), 3);
+		
+		inputs["data"] = input;
+
+		yield return null;
+
+		StartCoroutine(RunInference());
+	}
+	
+	IEnumerator RunInference ()
+	{
+		// Skip frame before starting
+		yield return null;
+		//displayImage.texture = inputImage;
+
+		while (repeatExecution-- > 0)
+		{
+			try
+			{
+				var start = Time.realtimeSinceStartup;
+				
+				Profiler.BeginSample("Schedule execution");
+				engine.Execute(inputs);
+				Profiler.EndSample();
+				
+				Profiler.BeginSample("Fetch execution results");
+				var output = engine.PeekOutput(output_name);
+				Profiler.EndSample();
+
+				var res = output.ArgMax()[0];
+				var end = Time.realtimeSinceStartup;
+				var label = labels[res];
+
+				if (label.Contains("tabby") && Mathf.Abs(output[res] - 0.4464f) < epsilon)
+				{
+					text.color = Color.green;
+					text.text = $"Success: {labels[res]} {output[res] * 100}%";
+				}
+				else
+				{
+					text.color = Color.red;
+					text.text = $"Failed: {labels[res]} {output[res] * 100}%";
+				}
+
+				UpdateAverage(end - start);
+				Debug.Log($"frametime = {(end - start)*1000f}ms, average = {averageDt * 1000}ms");
+			
+				
+			}
+			catch (Exception e)
+			{
+				Debug.Log($"Exception happened {e}");
+				//throw;
+			}
+			yield return null;
+		}
+	}
+	
+	// Scale image to target resolution and remap data from 0..1 to -1..1 on GPU
+	Texture PrepareTextureForInput(Texture2D src, bool needsCPUcopy)
+	{
+		var targetRT = RenderTexture.GetTemporary(inputResolutionX, inputResolutionY, 0, RenderTextureFormat.ARGB32);
+		RenderTexture.active = targetRT;
+		Graphics.Blit(src, targetRT);
+
+		if (!needsCPUcopy)
+			return targetRT;
+		
+		var  result = new Texture2D(targetRT.width, targetRT.height);
+		result.ReadPixels(new Rect(0,0, targetRT.width, targetRT.height), 0, 0);
+		result.Apply();
+
+		return result;
+	}
+
+	private void UpdateAverage(float newValue)
+	{
+		rawAverageDt = rawAverageDt * 0.9f + 0.1f * newValue;
+		
+		// Drop spikes above 20%
+		if (newValue < 1.2f * rawAverageDt)
+		{
+			averageDt = averageDt * 0.9f + 0.1f * newValue;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		engine?.Dispose();
+
+		foreach (var key in inputs.Keys)
+		{
+			inputs[key].Dispose();
+		}
+		
+		inputs.Clear();
+	}
+}
---
+++ Third Party Notices.md
@@ -0,0 +1,6 @@
+This package contains third-party software components governed by the license(s) indicated below:
+---------
+
+Component Name: squeezenet neural net
+License Type: Apache 2.0
+Source: https://github.com/onnx/models/tree/master/vision/classification/squeezenet
